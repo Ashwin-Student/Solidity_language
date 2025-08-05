bc07d22da92f23beda504cc2a13bbd6bdbb8cd77
Ashwin-Student <debreashvin@gmail.com>
2025-08-05 20:14:44 +0530

    Create do_while.sol

---
+++ do_while.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.8.0;
+
+contract Do_wjhile{
+
+    uint256 [] data;
+    uint8 j=0;
+
+    function loop() public returns (uint256[] memory){
+        do{
+            j++;
+            data.push(j);
+
+        }while(j<5);
+        return data;
+    }
+}
