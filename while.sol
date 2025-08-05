65c772052fdbeef4b2c1254a5874a31aea865e08
Ashwin-Student <debreashvin@gmail.com>
2025-08-05 20:15:03 +0530

    Create while.sol

---
+++ while.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+contract While{
+
+
+
+    uint256[] data;
+    uint8 k=0;
+    function loop() public returns(uint256[] memory){
+        while(k<5){
+            k++;
+            data.push(k);
+
+        }
+        return data;
+    } 
+
+}
